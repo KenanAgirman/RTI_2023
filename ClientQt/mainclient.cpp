7f8aea867f0b6ecbdfcbad8434e5b2dd568f1b4a
KenanAgirman <kenan.agirman@student.hepl.be>
2023-10-02 20:41:03 +0200

    probleme avec la librairie tcp

--- ClientQt/mainclient.cpp
+++ ClientQt/mainclient.cpp
@@ -1,13 +1,48 @@
 #include "windowclient.h"
+#include "../OVESP+SMOP/TCP.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
 
 #include <QApplication>
 
+void HandlerSIGINT(int);
+
 WindowClient *w;
+int sClient;
 
 int main(int argc, char *argv[])
 {
+    if(argc != 3)
+    {
+      printf("Erreur...\n");
+      printf("USAGE : ServeurTest portServeur\n");
+      exit(1);
+    }
+
+    struct sigaction A;
+    A.sa_flags = 0;
+    sigemptyset(&A.sa_mask);
+    A.sa_handler = HandlerSIGINT;
+    
+    if((sClient = ServerSocket(atoi(argv[1]))) == -1)
+    {
+      perror("Erreur de ServeurSocket");
+      exit(1);
+    }
+
+    printf("socket client = %d\n",sClient);
     QApplication a(argc, argv);
     w = new WindowClient();
     w->show();
     return a.exec();
+}
+void HandlerSIGINT(int n)
+{
+    printf("\nArret du client.\n");
+    close(sClient);
+    exit(0);
 }
