e7944438412f1f7236811b8b817203b9c4774d73
KenanAgirman <kenan.agirman@student.hepl.be>
2023-10-31 17:17:00 +0100

    :snowman:

--- RTI_JAVA/src/Main.java
+++ RTI_JAVA/src/Main.java
@@ -1,10 +1,16 @@
 import GUI.App;
 import com.formdev.flatlaf.FlatDarculaLaf;
+import Model.*;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FlatDarculaLaf.setup();
-        App app = new App();
-        app.setVisible(true);
+        //App app = new App();
+        model m1 = new model();
+        m1.Connect();
+
+        //app.setVisible(true);
     }
 }
